a97636c382aeb3fd1889b170fc4bfa6a988ea154
Mark Charles May <markcharlesmay@gmail.com>
2020-02-22 14:07:24 -0800

    progress bar function

--- wordcount.C
+++ wordcount.C
@@ -2,12 +2,16 @@
 #include <fstream>
 #include <iostream>
 #include <pthread.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 using namespace std;
 
+#define MAXSYM 40
 const char *separators = " \t";
 
 typedef struct {
-    long * CurrentStatus; // long which represents the current status of the computation being tracked
+    long *CurrentStatus; // long which represents the current status of the computation being tracked
     long InitialValue; //  starting value for the computation
     long TerminationValue; //  value at which the computation is complete
 } PROGRESS_STATUS;
@@ -15,16 +19,19 @@ typedef struct {
 void *progress_monitor(void* arg);
 long word_count(std::string fileName); //spawns progress bar thread 
 bool checkArgs(const char *argv[]);
-long file_size(std::string fileName); //byte size
+long get_file_size(std::string fileName); //byte size
 
 int main(int argc, const char *argv[])
 {
     std::string filePath = argv[1];
+    long num_words;
 
     if (checkArgs(argv))
     {
-        word_count(filePath);
+        num_words = word_count(filePath);
     }
+
+    cout << "There are " << num_words << " words in " << argv[1] << ".\n";
     
     return 0;
 }
@@ -43,12 +50,12 @@ bool checkArgs(const char *argv[])
 long word_count(std::string fileName)
 { //spawns progress bar thread 
 
-    long fsize = file_size(fileName);
     PROGRESS_STATUS* pstat = new PROGRESS_STATUS;
 
     pstat->InitialValue = 0;
-    pstat->TerminationValue = fsize;
-    pstat->CurrentStatus = 0;
+    pstat->TerminationValue = get_file_size(fileName);
+    cout << "File size in bytes is " << get_file_size(fileName) << endl;
+    pstat->CurrentStatus = new long(0);
 
     pthread_t progress;
     pthread_create(&progress, NULL, progress_monitor, (void *)pstat);
@@ -57,17 +64,17 @@ long word_count(std::string fileName)
     long c_count = 0;
     //Read in valid word file
     std::ifstream wordFile(fileName);
-    char c;
+    char c = NULL;
     char curr = NULL;
     char prev = NULL;
     while (wordFile.get(c)){
         curr = c;
         c_count++;
+        *(pstat->CurrentStatus) += c_count;
 
         if(!isspace(curr))
             if(isspace(prev) || prev == NULL){ //NULL for first char in a stream
                 w_count++;
-                pstat->CurrentStatus+= c_count;
             }
 
         prev = curr;
@@ -75,33 +82,49 @@ long word_count(std::string fileName)
     }        // loop getting single characters
 
 
-    wordFile.close();   
 
     pthread_join(progress, NULL);
+    delete pstat->CurrentStatus;
+    delete pstat;
+    wordFile.close();   
 
     return w_count;
 }
 
-long file_size(std::string fileName){
-    long count = 0;
-
-    //Read in valid word file
-    std::ifstream wordFile(fileName);
-
-    char c;
-    
-    while (wordFile.get(c))   // loop getting single characters
-        count++;
-
-    wordFile.close();   
-
-    return count;
+long get_file_size(std::string fileName){
+    struct stat buff;
+    int size = stat(fileName.c_str(), &buff);
+    return size == 0 ? buff.st_size : -1;
 }
 
 void * progress_monitor(void* pstat){
     PROGRESS_STATUS* progstat = (PROGRESS_STATUS *) pstat;
-    //if word count changes then print new progress
+    //every time the char count changes then print new progress
     //print the progress bar
+    //cout << "In the thread." << endl;
 
+    long term_val = progstat->TerminationValue;
+    long init_val = progstat->InitialValue;
 
+    while (*(progstat->CurrentStatus) <= term_val) {
+        long progress = (*(progstat->CurrentStatus) - init_val);
+        long total = term_val - init_val;
+        int num_symbols = ((progress / total) * MAXSYM);
+        cout << "Progress: " << progress << ", Total: " << total << ", Num_symbols: " << num_symbols << endl;
+        int i;
+        //string loadbar = "";
+        for (i = 1; i <= num_symbols; i++) {
+            if (i % 10 == 0) 
+                //loadbar += "+";
+                cout << "+";
+            else
+                //loadbar += "-";
+                cout << "-";
+        }
+        cout << flush;
+        if (num_symbols == MAXSYM) {
+            cout << "\nOut of the thread." << endl;
+            pthread_exit(NULL);
+        }
+    }
 }
